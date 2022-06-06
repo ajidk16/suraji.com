a06227f76cb04778680e3ef4bacea5827410cb7d
Surajidk <surajidk12@gmail.com>
2022-06-06 13:47:21 +0700

    custom text header head

--- components/Atoms/Header.js
+++ components/Atoms/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header({ className, title, subtitle }) {
   return (
     <div className={`mb-8 ${className}`}>
-      <div className="text-xl sm:text-3xl lg:text-4xl capitalize mb-2">{title}</div>
+      <div className="text-2xl sm:text-3xl lg:text-4xl capitalize mb-2">{title}</div>
       <p>{subtitle}</p>
     </div>
   );
