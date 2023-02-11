27d5f8484d7bec5777fa8132801ae363f12017fc
suraji <surajidk12@gmail.com>
2023-02-11 16:55:48 +0700

    tidy up

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -28,6 +28,7 @@ export default function Blog({ blog }) {
             / {format(parseISO(blog?.date), "MMMM d, yyyy")}
           </div>
           {/* <div>4 min red • bendera</div> */}
+          
         </div>
         <BlogGithub content={blog?.content} />
       </div>
